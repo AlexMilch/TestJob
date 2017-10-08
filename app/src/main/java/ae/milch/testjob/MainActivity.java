d567a19a1898078cbe67941bc175b223d69e18ff
AlexME93 <milchakovae@gmail.com>
2017-10-08 20:35:19 +0700

    add fragments for cards and my contact, add botton navigation view

--- app/src/main/java/ae/milch/testjob/MainActivity.java
+++ app/src/main/java/ae/milch/testjob/MainActivity.java
@@ -1,13 +1,37 @@
 package ae.milch.testjob;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new
+            BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.action_cards:
+                            getSupportFragmentManager().beginTransaction().replace(R.id.content, new CardsFragment()).commit();
+                            return true;
+                        case R.id.action_contacts:
+                            getSupportFragmentManager().beginTransaction().replace(R.id.content, new MyContactFragment()).commit();
+                            return true;
+                    }
+                    return false;
+                }
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction().add(R.id.content, new CardsFragment()).commit();
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 }
